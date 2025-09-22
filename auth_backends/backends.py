e099d04128e44ac62b4e140b2bdfd0715e1bc5e3
Robert Raposa <rraposa@gmail.com>
2025-09-22 15:40:33 -0400

    fixup! Update auth_backends/backends.py

--- auth_backends/backends.py
+++ auth_backends/backends.py
@@ -128,7 +128,7 @@ class EdXOAuth2(BaseOAuth2):
 
             logout(request)
 
-            # .. custom_attribute_name: start.session_cleanup_performed
+            # .. custom_attribute_name: session_cleanup.logout_performed
             # .. custom_attribute_description: Indicates that session cleanup was
             #    actually performed during OAuth start.
             set_custom_attribute('session_cleanup.logout_performed', True)
